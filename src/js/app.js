1f9925bc742c166e08d107ca95e40cdbf226593f
nickjevershed <nick.evershed@gmail.com>
2020-04-24 14:36:41 +1000

    updates

--- src/js/app.js
+++ src/js/app.js
@@ -2,7 +2,7 @@ import template from '../templates/template.html'
 import Ractive from 'ractive'
 import * as d3 from "d3"
 
-function init(country, confirmed, confirmed_daily, deaths, recovered, aus, overrides, latest) {
+function init(country, confirmed_daily, aus, overrides, latest, aus_daily) {
 
 	// console.log(confirmed)
 	// console.log(confirmed_daily)
@@ -60,44 +60,48 @@ function init(country, confirmed, confirmed_daily, deaths, recovered, aus, overr
 
 	var ausManualConfirmed = parseInt(aus.sheets['latest totals'][8]['Confirmed cases (cumulative)'])
 	var ausManualDeaths = parseInt(aus.sheets['latest totals'][8]['Deaths'])
+	var ausManualRecovered = parseInt(aus.sheets['latest totals'][8]['Recovered'])
+	var ausManualActive = parseInt(aus.sheets['latest totals'][8]['Active cases'])
 
 	var ausManualConfirmed2 = parseInt(overrides.sheets['Sheet1'][3]['Cases'])
 	var ausManualDeaths2 = parseInt(overrides.sheets['Sheet1'][3]['Deaths'])
 	var ausManualRecovered2 = parseInt(overrides.sheets['Sheet1'][3]['Recovered'])
-	
+		
+
+
 	// var ausAutoConfirmed = latest.filter(d => d.Country_Region == "Australia")[0]['Confirmed']
 	// var ausAutoDeaths = latest.filter(d => d.Country_Region == "Australia")[0]['Deaths']
 
 	var totalManualConfirmed = niceNumber(overrides.sheets['Sheet1'][0]['Cases'])
 	var totalManualDeaths = niceNumber(overrides.sheets['Sheet1'][0]['Deaths'])
-	var totalManualRecovered = niceNumber(overrides.sheets['Sheet1'][0]['Recovered'])
+	// var totalManualRecovered = niceNumber(overrides.sheets['Sheet1'][0]['Recovered'])
 
 	var totalAutoConfirmed = latest.filter(d => d.Country_Region == "Total")[0]['Confirmed']
 	var totalAutoDeaths = latest.filter(d => d.Country_Region == "Total")[0]['Deaths']
-	var totalAutoRecovered = latest.filter(d => d.Country_Region == "Total")[0]['Recovered']
+	// var totalAutoRecovered = latest.filter(d => d.Country_Region == "Total")[0]['Recovered']
 
 	var usManualConfirmed = niceNumber(overrides.sheets['Sheet1'][1]['Cases'])
 	var usManualDeaths = niceNumber(overrides.sheets['Sheet1'][1]['Deaths'])
-	var usManualRecovered = niceNumber(overrides.sheets['Sheet1'][1]['Recovered'])
+	// var usManualRecovered = niceNumber(overrides.sheets['Sheet1'][1]['Recovered'])
 
 	var usAutoConfirmed = latest.filter(d => d.Country_Region == "US")[0]['Confirmed']
 	var usAutoDeaths = latest.filter(d => d.Country_Region == "US")[0]['Deaths']
-	var usAutoRecovered = latest.filter(d => d.Country_Region == "US")[0]['Recovered']
+	// var usAutoRecovered = latest.filter(d => d.Country_Region == "US")[0]['Recovered']
 
 	var ukManualConfirmed = niceNumber(overrides.sheets['Sheet1'][2]['Cases'])
 	var ukManualDeaths = niceNumber(overrides.sheets['Sheet1'][2]['Deaths'])
-	var ukManualRecovered = niceNumber(overrides.sheets['Sheet1'][2]['Recovered'])
+	// var ukManualRecovered = niceNumber(overrides.sheets['Sheet1'][2]['Recovered'])
 
 	var ukAutoConfirmed = latest.filter(d => d.Country_Region == 'United Kingdom')[0]['Confirmed']
 	var ukAutoDeaths = latest.filter(d => d.Country_Region == 'United Kingdom')[0]['Deaths']
-	var ukAutoRecovered = latest.filter(d => d.Country_Region == 'United Kingdom')[0]['Recovered']
+	// var ukAutoRecovered = latest.filter(d => d.Country_Region == 'United Kingdom')[0]['Recovered']
 
 	var timeFormat = d3.timeFormat('%Y-%m-%d')
 	var timeParse = d3.timeParse('%d/%m/%Y')
 	var ausManualTimestamp = aus.sheets['latest totals'][8]['Last updated']
-	var autoTimestamp = confirmed[confirmed.length-1]['index']
+	var autoTimestamp = confirmed_daily[confirmed_daily.length-1]['index']
 
-	var ausFinalDeaths, ausFinalConfirmed, totalFinalDeaths, totalFinalConfirmed, totalFinalRecovered, usFinalDeaths, usFinalConfirmed, usFinalRecovered, ukFinalDeaths, ukFinalConfirmed, ukFinalRecovered
+	var ausFinalDeaths, ausFinalConfirmed, ausFinalRecovered, totalFinalDeaths, totalFinalConfirmed, totalFinalRecovered, usFinalDeaths, usFinalConfirmed, usFinalRecovered, ukFinalDeaths, ukFinalConfirmed, ukFinalRecovered
 
 	// console.log(ausManualConfirmed,ausAutoConfirmed)
 
@@ -117,34 +121,35 @@ function init(country, confirmed, confirmed_daily, deaths, recovered, aus, overr
 
 	ausFinalConfirmed = compare([ausManualConfirmed, ausManualConfirmed2])
 	ausFinalDeaths = compare([ausManualDeaths, ausManualDeaths2])
+	ausFinalRecovered = compare([ausManualRecovered, ausManualRecovered2])
 
 	totalFinalConfirmed = compare([totalManualConfirmed, totalAutoConfirmed])
 	totalFinalDeaths = compare([totalManualDeaths, totalAutoDeaths])
-	totalFinalRecovered = compare([totalManualRecovered, totalAutoRecovered])
+	// totalFinalRecovered = compare([totalManualRecovered, totalAutoRecovered])
 
 	usFinalConfirmed = compare([usManualConfirmed, usAutoConfirmed])
 	usFinalDeaths = compare([usManualDeaths, usAutoDeaths])
-	usFinalRecovered = compare([usManualRecovered, usAutoRecovered])
+	// usFinalRecovered = compare([usManualRecovered, usAutoRecovered])
 
 	ukFinalConfirmed = compare([ukManualConfirmed, ukAutoConfirmed])
 	ukFinalDeaths = compare([ukManualDeaths, ukAutoDeaths])
-	ukFinalRecovered = compare([ukManualRecovered, ukAutoRecovered])
+	// ukFinalRecovered = compare([ukManualRecovered, ukAutoRecovered])
 
-	data["Australia"]['confirmed'] = format(ausFinalConfirmed)
+	data["Australia"]['confirmed'] = format(ausManualActive)
 	data["Australia"]['deaths'] = format(ausFinalDeaths)
-	data["Australia"]['recovered'] = format(recovered[recovered.length-1]['Australia'])
+	data["Australia"]['recovered'] = format(ausFinalRecovered)
 
 	data["United Kingdom"]['confirmed'] = format(ukFinalConfirmed)
 	data["United Kingdom"]['deaths'] = format(ukFinalDeaths)
-	data["United Kingdom"]['recovered'] = format(ukFinalRecovered)
+	// data["United Kingdom"]['recovered'] = format(ukFinalRecovered)
 
 	data["US"]['confirmed'] = format(usFinalConfirmed)
 	data["US"]['deaths'] = format(usFinalDeaths)
-	data["US"]['recovered'] = format(usFinalRecovered)
+	// data["US"]['recovered'] = format(usFinalRecovered)
 
 	data["Total"]['confirmed'] = format(totalFinalConfirmed)
 	data["Total"]['deaths'] = format(totalFinalDeaths)
-	data["Total"]['recovered'] = format(totalFinalRecovered)
+	// data["Total"]['recovered'] = format(totalFinalRecovered)
 
 
 	// console.log(data);
@@ -154,6 +159,8 @@ function init(country, confirmed, confirmed_daily, deaths, recovered, aus, overr
 			template: template,
 			data: { 
 					location:data[country],
+					layout: "three",
+					label: "Active cases*",
 					ausManualTimestamp:ausManualTimestamp,
 					autoTimestamp:autoTimestamp
 				}
@@ -163,6 +170,8 @@ function init(country, confirmed, confirmed_daily, deaths, recovered, aus, overr
 	ractive.on({
 		world: function ( event ) {
 			ractive.set('location', data["Total"])
+			ractive.set('layout', "two")
+			ractive.set('label', "Confirmed cases")
 			country = 'Total'
 			d3.selectAll(".btn").classed("btn-selected", false);
 			d3.select(".world").classed("btn-selected", true);
@@ -171,6 +180,8 @@ function init(country, confirmed, confirmed_daily, deaths, recovered, aus, overr
 		us: function ( event ) {
 			console.log("us")
 			ractive.set('location', data["US"])
+			ractive.set('layout', "two")
+			ractive.set('label', "Confirmed cases")
 			country = 'US'
 			d3.selectAll(".btn").classed("btn-selected", false);
 			d3.select(".us").classed("btn-selected", true);
@@ -179,6 +190,8 @@ function init(country, confirmed, confirmed_daily, deaths, recovered, aus, overr
 		uk: function ( event ) {
 			console.log("uk")
 			ractive.set('location', data["United Kingdom"])
+			ractive.set('layout', "two")
+			ractive.set('label', "Confirmed cases")
 			country = 'United Kingdom'
 			d3.selectAll(".btn").classed("btn-selected", false);
 			d3.select(".uk").classed("btn-selected", true);
@@ -187,6 +200,8 @@ function init(country, confirmed, confirmed_daily, deaths, recovered, aus, overr
 		aus: function ( event ) {
 			console.log("aus")
 			ractive.set('location', data["Australia"])
+			ractive.set('layout', "three")
+			ractive.set('label', "Active cases*")
 			country = 'Australia'
 			d3.selectAll(".btn").classed("btn-selected", false);
 			d3.select(".aus").classed("btn-selected", true);
@@ -200,16 +215,14 @@ function init(country, confirmed, confirmed_daily, deaths, recovered, aus, overr
 };
 
 Promise.all([
-	d3.json('https://interactive.guim.co.uk/2020/03/coronavirus-widget-data/confirmed.json'),
-	d3.json('https://interactive.guim.co.uk/2020/03/coronavirus-widget-data/confirmed_daily.json'),
-	d3.json('https://interactive.guim.co.uk/2020/03/coronavirus-widget-data/deaths.json'),
-	d3.json('https://interactive.guim.co.uk/2020/03/coronavirus-widget-data/recovered.json'),
-	d3.json('https://interactive.guim.co.uk/docsdata/1q5gdePANXci8enuiS4oHUJxcxC13d6bjMRSicakychE.json'),
-	d3.json('https://interactive.guim.co.uk/docsdata/1jy3E-hIVvbBAyUx7SY3IfUADB85mGoaR2tobYu9iifA.json'),
-	d3.json("https://interactive.guim.co.uk/2020/03/coronavirus-widget-data/latest.json")
-])
+		d3.json('https://interactive.guim.co.uk/2020/03/coronavirus-widget-data/confirmed_daily.json'),
+		d3.json('https://interactive.guim.co.uk/docsdata/1q5gdePANXci8enuiS4oHUJxcxC13d6bjMRSicakychE.json'),
+		d3.json('https://interactive.guim.co.uk/docsdata/1jy3E-hIVvbBAyUx7SY3IfUADB85mGoaR2tobYu9iifA.json'),
+		d3.json("https://interactive.guim.co.uk/2020/03/coronavirus-widget-data/latest.json"),
+		d3.json("https://interactive.guim.co.uk/docsdata/1xEijW_9nGVP55-CtmMSyIdOYWP9YYwbs-xZYoAeHEso.json")
+	])
 .then((results) =>  {
-	init('Australia', results[0], results[1], results[2], results[3], results[4], results[5], results[6])
+	init('Australia', results[0], results[1], results[2], results[3], results[4])
 })
 
 
